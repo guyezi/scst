d2d7a98852327372a4dd7a5b3cde44ff5eddab1d
bvassche <bvassche@d57e44dd-8a1f-0410-8b47-8ef2f437770f>
2011-03-23 18:33:39 +0000

    SCSI REPORT TARGET GROUPS protocol conformance fix: don't include the RETURN DATA LENGTH field size in the field itself.

--- branches/alua/scst/src/scst_tg.c
+++ branches/alua/scst/src/scst_tg.c
@@ -34,26 +34,24 @@ __lookup_tg_dev(struct scst_target_group *tg, struct scst_device *dev)
 /**
  * scst_tg_get_group_info() - Build REPORT TARGET GROUPS response.
  * @buf: Pointer to a pointer to which the result buffer pointer will be set.
- * @response_length: Response length.
+ * @length: Response length, including the "RETURN DATA LENGTH" field.
  * @dev: Pointer to the SCST device for which to obtain group information.
  * @data_format: Three-bit response data format specification.
  */
-int scst_tg_get_group_info(void **buf, uint32_t *response_length,
+int scst_tg_get_group_info(void **buf, uint32_t *length,
 			   struct scst_device *dev, uint8_t data_format)
 {
 	struct scst_target_group *tg;
 	struct scst_target_group_dev *tgdev;
 	struct scst_tgt *tgt;
 	uint8_t *p;
+	uint32_t ret_data_len;
 	int res;
 
 	BUG_ON(!buf);
-	BUG_ON(!response_length);
+	BUG_ON(!length);
 
-	*response_length = 0;
-
-	/* Return data length. */
-	*response_length += 4;
+	ret_data_len = 0;
 
 	res = -EINVAL;
 	switch (data_format) {
@@ -61,7 +59,7 @@ int scst_tg_get_group_info(void **buf, uint32_t *response_length,
 		break;
 	case 1:
 		/* Extended header */
-		*response_length += 4;
+		ret_data_len += 4;
 		break;
 	default:
 		goto out;
@@ -71,21 +69,23 @@ int scst_tg_get_group_info(void **buf, uint32_t *response_length,
 
 	list_for_each_entry(tg, &scst_target_group_list, entry) {
 		/* Target port group descriptor header. */
-		*response_length += 8;
+		ret_data_len += 8;
 		list_for_each_entry(tgt, &tg->tgt_list, target_group_entry) {
 			/* Target port descriptor. */
-			*response_length += 4;
+			ret_data_len += 4;
 		}
 	}
 
+	*length = ret_data_len + 4;
+
 	res = -ENOMEM;
-	*buf = kzalloc(*response_length, GFP_KERNEL);
+	*buf = kzalloc(*length, GFP_KERNEL);
 	if (!*buf)
 		goto out_unlock;
 
 	p = *buf;
 	/* Return data length. */
-	put_unaligned(cpu_to_be32(*response_length), (__be32 *)p);
+	put_unaligned(cpu_to_be32(ret_data_len), (__be32 *)p);
 	p += 4;
 	if (data_format == 1) {
 		/* Extended header */
@@ -119,7 +119,7 @@ int scst_tg_get_group_info(void **buf, uint32_t *response_length,
 		}
 	}
 
-	WARN_ON(p - (uint8_t *)*buf != *response_length);
+	WARN_ON(p - (uint8_t *)*buf != *length);
 
 	res = 0;
 
