guyezi/scst

commit a65843ec45b3a711e4208707c3d493cb055183cb

qla2x00t-32gbit, target: Fix W=1 warnings
git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8093 d57e44dd-8a1f-0410-8b47-8ef2f437770f

qla2x00t-32gbit/qla2x00-target/qla_tgt.c
@@ -48,7 +48,7 @@ extern struct mutex qla_tgt_mutex;
 extern struct list_head qla_tgt_glist;
 
 
-size_t
+static size_t
 qlt_add_vtarget(u64 port_name, u64 node_name, u64 parent_host)
 {
 	struct Scsi_Host *shost = NULL;
@@ -89,7 +89,7 @@ qlt_add_vtarget(u64 port_name, u64 node_name, u64 parent_host)
 }
 EXPORT_SYMBOL(qlt_add_vtarget);
 
-size_t qlt_del_vtarget(u64 port_name)
+static size_t qlt_del_vtarget(u64 port_name)
 {
 	struct qla_tgt *tgt, *t;
 	scsi_qla_host_t *vha = NULL;

qla2x00t-32gbit/qla2x00-target/scst_qla2xxx.c
@@ -233,7 +233,7 @@ struct cmd_state_name {
 	{QLA_TGT_STATE_PROCESSED, "Processed"},
 };
 
-char* cmdstate_to_str(uint8_t state)
+static char *cmdstate_to_str(uint8_t state)
 {
 	int i;
 	struct cmd_state_name *e;
@@ -441,7 +441,7 @@ static void sqa_qla2xxx_rel_cmd(struct qla_tgt_cmd *cmd)
 	percpu_ida_free(&sqa_tgt->tgt_tag_pool, cmd->se_cmd.map_tag);
 }
 
-struct qla_tgt_cmd *sqa_qla2xxx_get_cmd(struct fc_port *sess)
+static struct qla_tgt_cmd *sqa_qla2xxx_get_cmd(struct fc_port *sess)
 {
 	struct sqa_scst_tgt *sqa_tgt =
 		(struct sqa_scst_tgt*)sess->vha->vha_tgt.target_lport_ptr;

qla2x00t-32gbit/qla_target.c
@@ -4424,7 +4424,7 @@ static int qlt_issue_task_mgmt(struct fc_port *sess, u64 lun,
 	case QLA_TGT_CLEAR_TS:
 	case QLA_TGT_ABORT_TS:
 		abort_cmds_for_lun(vha, lun, a->u.isp24.fcp_hdr.s_id);
-		/* drop through */
+		/* fall through */
 	case QLA_TGT_CLEAR_ACA:
 		h = qlt_find_qphint(vha, mcmd->unpacked_lun);
 		mcmd->qpair = h->qpair;

commit 4aa6fcedff78edfb6660a34940b8faa88b898f31

qla2x00t-32gbit, target: Fix an "undefined symbol" error
Fixes: commit a65843ec45b3 ("qla2x00t-32gbit, target: Fix W=1 warnings" / r8093)

qla2x00t-32gbit/qla2x00-target/qla_tgt.c
@@ -43,13 +43,13 @@
 #include <scsi/scsi_tcq.h>
 
 #include "qla_def.h"
+#include "scst_qla2xxx.h"
 
 extern struct mutex qla_tgt_mutex;
 extern struct list_head qla_tgt_glist;
 
 
-static size_t
-qlt_add_vtarget(u64 port_name, u64 node_name, u64 parent_host)
+size_t qlt_add_vtarget(u64 port_name, u64 node_name, u64 parent_host)
 {
 	struct Scsi_Host *shost = NULL;
 	struct qla_tgt *tgt;
@@ -89,7 +89,7 @@ qlt_add_vtarget(u64 port_name, u64 node_name, u64 parent_host)
 }
 EXPORT_SYMBOL(qlt_add_vtarget);
 
-static size_t qlt_del_vtarget(u64 port_name)
+size_t qlt_del_vtarget(u64 port_name)
 {
 	struct qla_tgt *tgt, *t;
 	scsi_qla_host_t *vha = NULL;