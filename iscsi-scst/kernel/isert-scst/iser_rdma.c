guyezi/scst

commit 36c0648b7e71d9de49207a6aabaec7d62957d02f

isert-scst: Fix the build against kernel v5.3
In kernel v5.3 the ib_destroy_cq() return type has been changed from 'int'
into 'void'.


git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8475 d57e44dd-8a1f-0410-8b47-8ef2f437770f

iscsi-scst/kernel/isert-scst/iser_rdma.c
@@ -1144,7 +1144,7 @@ out:
 
 static void isert_device_release(struct isert_device *isert_dev)
 {
-	int err, i;
+	int i;
 
 	TRACE_ENTRY();
 
@@ -1165,10 +1165,7 @@ static void isert_device_release(struct isert_device *isert_dev)
 		cancel_work_sync(&cq_desc->cq_comp_work);
 #endif
 
-		err = ib_destroy_cq(cq_desc->cq);
-		if (unlikely(err))
-			PRINT_ERROR("Failed to destroy cq, err:%d", err);
-
+		ib_destroy_cq(cq_desc->cq);
 		destroy_workqueue(cq_desc->cq_workqueue);
 	}
 

commit 8878e4eb10ee029c20f8e2b1a1e4bfbdf3fc216c

isert-scst: Unbreak the build for kernel versions <= v4.8
Fixes: 36c0648b7e71 ("isert-scst: Fix the build against kernel v5.3") / r8475.

iscsi-scst/kernel/isert-scst/iser_rdma.c
@@ -1144,6 +1144,9 @@ out:
 
 static void isert_device_release(struct isert_device *isert_dev)
 {
+#ifndef IB_PD_HAS_LOCAL_DMA_LKEY
+	int err;
+#endif
 	int i;
 
 	TRACE_ENTRY();