guyezi/scst

commit 6ab417a5ef0561963c4abf1ab42ba176d612d1f9

qla2x00t-32gbit: Always check the qla2x00_wait_for_hba_online() return value
This patch avoids that Coverity complains that the return value of
qla2x00_wait_for_hba_online() is not always checked.


git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8381 d57e44dd-8a1f-0410-8b47-8ef2f437770f

qla2x00t-32gbit/qla_target.c
@@ -6692,7 +6692,8 @@ qlt_enable_vha(struct scsi_qla_host *vha)
 	} else {
 		set_bit(ISP_ABORT_NEEDED, &base_vha->dpc_flags);
 		qla2xxx_wake_dpc(base_vha);
-		qla2x00_wait_for_hba_online(base_vha);
+		WARN_ON_ONCE(qla2x00_wait_for_hba_online(base_vha) !=
+			     QLA_SUCCESS);
 	}
 	mutex_unlock(&ha->optrom_mutex);
 }
@@ -6723,7 +6724,7 @@ static void qlt_disable_vha(struct scsi_qla_host *vha)
 
 	set_bit(ISP_ABORT_NEEDED, &vha->dpc_flags);
 	qla2xxx_wake_dpc(vha);
-	qla2x00_wait_for_hba_online(vha);
+	WARN_ON_ONCE(qla2x00_wait_for_hba_online(vha) != QLA_SUCCESS);
 }
 
 /*

qla2x00t-32gbit/qla2x00-target/scst_qla2xxx.c
@@ -1170,7 +1170,7 @@ static ssize_t sqa_abort_isp_store(struct kobject *kobj,
 		   tgt->vha->vp_idx);
 
 	set_bit(ISP_ABORT_NEEDED, &tgt->vha->dpc_flags);
-	qla2x00_wait_for_hba_online(tgt->vha);
+	WARN_ON_ONCE(qla2x00_wait_for_hba_online(tgt->vha) != QLA_SUCCESS);
 
 	return size;
 #endif

qla2x00t-32gbit/qla_attr.c
@@ -726,7 +726,8 @@ qla2x00_sysfs_write_reset(struct file *filp, struct kobject *kobj,
 			break;
 		} else {
 			/* Make sure FC side is not in reset */
-			qla2x00_wait_for_hba_online(vha);
+			WARN_ON_ONCE(qla2x00_wait_for_hba_online(vha) !=
+				     QLA_SUCCESS);
 
 			/* Issue MPI reset */
 			scsi_block_requests(vha->host);

commit 48c545981da4aa0c78680155d52ce3135deea7a3

qla2x00t-32gbit: Suppress a recently introduced kernel warning
Fixes: 6ab417a5ef05 ("qla2x00t-32gbit: Always check the qla2x00_wait_for_hba_online() return value").

qla2x00t-32gbit/qla_target.c
@@ -6721,7 +6721,9 @@ static void qlt_disable_vha(struct scsi_qla_host *vha)
 
 	set_bit(ISP_ABORT_NEEDED, &vha->dpc_flags);
 	qla2xxx_wake_dpc(vha);
-	WARN_ON_ONCE(qla2x00_wait_for_hba_online(vha) != QLA_SUCCESS);
+	if (qla2x00_wait_for_hba_online(vha) != QLA_SUCCESS)
+		ql_dbg(ql_dbg_tgt, vha, 0xe081,
+		       "qla2x00_wait_for_hba_online() failed\n");
 }
 
 /*